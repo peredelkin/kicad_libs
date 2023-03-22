205f0cda95f80374674fc3c4927935222ddf09cf
peredelkin <ep8kha@gmail.com>
2023-03-22 17:02:57 +0500

    даташит и часть расчета

---
+++ TPS54x3x/TPS54332.m
@@ -0,0 +1,90 @@
+Vref = 0.8;
+Vggm = 800;
+Vout = 2.5;
+Voutmax = Vout + 0.02;
+Ven = 1.25;
+Vstop = 4;
+Vstart = 5;
+Vin = 5;
+Vinmax = 15;
+
+Ien1 = 1*10^-6;
+Ien2 = 3*10^-6;
+Iss = 2*10^-6;
+Tss = 5*10^-3;
+Rvsns1 = 10*10^3;
+Ioutmax = 3.5;
+
+Cbulk = 10*10^-6;
+ESRbulk = 3*10^-3;
+
+Cout = 47*10^-6;
+ESRout = 3*10^-3;
+Nc = 2;
+
+Fsw = 1*10^6;
+Fc0max = 75*10^3;
+
+Kind = 0.3;
+
+Lout = 2.5*10^-6;
+
+D = Vout/Vin;
+
+Voppmax = 10*10^-3;
+
+Roa = 8.696*10^6;
+
+Ren1 = (Vstart - Vstop)/Ien2;
+Ren2 = Ven/(((Vstart - Ven)/Ren1) + Ien1);
+printf("Ren1 = %d\n", Ren1);
+printf("Ren2 = %d\n", Ren2);
+
+Css = (Tss * Iss)/Vref;
+printf("Css = %d\n", Css);
+
+Rvsns2 = (Rvsns1 * Vref) / (Vout - Vref);
+printf("Rvsns1 = %d\n", Rvsns1);
+printf("Rvsns2 = %d\n", Rvsns2);
+
+Vinripple = (Ioutmax * 0.25) / (Cbulk * Fsw * 0.8) + (Ioutmax * ESRbulk);
+printf("Vinripple = %d\n", Vinripple);
+
+Iripple = Ioutmax / 2;
+printf("Iripple = %d\n", Iripple);
+
+Lmin = (Voutmax * (Vinmax - Vout))/(Vinmax * Kind * Ioutmax * Fsw * 0.8);
+printf("Lmin = %d\n", Lmin);
+
+Ilpp = (Vout * (Vinmax - Vout)) / (Vinmax * Lout * Fsw * 0.8);
+printf("Ilpp = %d\n", Ilpp);
+
+Ilrms = sqrt(Ioutmax^2 + (((Vout * (Vinmax - Vout))/(Vinmax * Lout * Fsw * 0.8))^2)/12);
+printf("Ilrms = %d\n", Ilrms);
+
+Ilpk = Ioutmax + ((Vout * (Vinmax - Vout))/(1.6 * Vinmax * Lout * Fsw));
+printf("Ilpk = %d\n", Ilpk);
+
+Coutmin = 1/(2*pi*(Vout/Ioutmax)*Fc0max);
+printf("Coutmin = %d\n", Coutmin);
+
+#дальше какая-то хуйня
+Vopp = Ilpp*((D-0.5)/(4*Fsw*Cout) + ESRout);
+printf("Vopp = %d\n", Vopp);
+
+ESRmax = (Voppmax/Ilpp) - ((D-0.5)/(4*Fsw*Cout));
+printf("ESRmax = %d\n", ESRmax);
+
+Icoutrms = (1/sqrt(12))*((Vout * (Vinmax - Vout))/(Vinmax * Lout * Fsw * Nc));
+printf("Icoutrms = %d\n", Icoutrms);
+
+Gdc = (Vggm * Vref) / Vout;
+printf("Gdc = %d\n", Gdc);
+
+
+
+
+
+
+
+
