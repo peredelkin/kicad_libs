4121680183f6f460f5972ef1c7ca18fe5c541702
peredelkin <ep8kha@gmail.com>
2023-03-27 15:39:38 +0500

    mp2307

--- MP2307/MP2307.m
+++ MP2307/MP2307.m
@@ -9,9 +9,9 @@ L = (Vout/(Fs * dIL)) * (1 - Vout/Vin)
 Iload = 3;
 Ilp = Iload + (Vout/(2 * Fs * L)) * (1 - Vout/Vin)
 Ic1 = Iload * sqrt((Vout/Vin) * (1 - Vout/Vin))
-C1 = 0.8 * 2 * 22e-6;
+C1 = 2 * 22e-6;
 dVin = (Iload/(C1 * Fs)) * (Vout/Vin) * (1 - Vout/Vin)
-C2 = 0.8 * 2 * 47e-6;
+C2 = 2 * 47e-6;
 Resr = 0.001;
 dVout = (Vout/(Fs * L)) * (1 - Vout/Vin) * (Resr + 1/(8 * Fs * C2))
 Rload = Vout/Iload
@@ -22,13 +22,13 @@ Gea = 820e-6;
 Fc = Fs * 0.1;
 
 R3 = (2 * pi * C2 * Fc)/(Gea * Gcs) * (Vout/Vref)
-R3 = 5.1e3
+R3 = 6.2e3
 
 C3 = 4/(2 * pi * R3 * Fc)
-C3 = 4.7e-9
+C3 = 3.3e-9
 
 C6 = (C2 * Resr)/R3
-C6 = 15e-12
+C6 = 22e-12
 
 Fp1 = Gea/(2 * pi * C3 * Aea)
 Fp2 = 1/(2 * pi * C2 * Rload)
